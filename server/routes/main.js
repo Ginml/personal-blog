9dbdce5332d2bb6151cc7a528e276d31dcad8bcc
ginml <199657461+Ginml@users.noreply.github.com>
2025-05-18 17:01:24 +0300

    Fix error in viewing posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -46,25 +46,25 @@ router.get('', async (req, res) => {
  */
 
 router.get('/post/:id', async (req, res) => {
-try{
+  try {
+    let slug = req.params.id;
 
-let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
 
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    }
 
+    res.render('post', { 
+      locals,
+      data,
+      currentRoute: `/post/${slug}`
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-
-const data = await post.findById({_id:slug});
-
-const locals = {
-  title: data.title,
-  description:"Simple Blog created with NodeJs, Express & MongoDb "
-}
-
-res.render('post', {locals, data});
-}
-catch(error){
-  console.log(error);
-}
 });
 
 
@@ -92,7 +92,8 @@ router.post('/search', async (req, res) => {
 
     res.render("search", {
     data,
-    locals
+    locals,
+    currentRoute: '/'
     });
 
 
@@ -102,63 +103,11 @@ router.post('/search', async (req, res) => {
 });
 
 
-
+// GET /About
 router.get('/about', (req, res) => {
   res.render('about', {
     currentRoute: '/about'
   });
-});
-
-
-
-
-//add comment from here to down there (check message down) this is to make sure posting and dating functionality works //
-function insertPostData () {
-    Post.insertMany([
-        {
-            title: "Building APIs with Node.js",
-            body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
-        },
-        {
-            title: "Deployment of Node.js applications",
-            body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
-        },
-        {
-            title: "Authentication and Authorization in Node.js",
-            body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
-        },
-        {
-            title: "Understand how to work with MongoDB and Mongoose",
-            body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
-        },
-        {
-            title: "build real-time, event-driven applications in Node.js",
-            body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
-        },
-        {
-            title: "Discover how to use Express.js",
-            body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
-        },
-        {
-            title: "Asynchronous Programming with Node.js",
-            body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
-        },
-        {
-        title: "Learn the basics of Node.js and its architecture",
-       body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
-     },
-     {
-       title: "NodeJs Limiting Network Traffic",
-       body: "Learn how to limit netowrk traffic."
-     },
-     {
-       title: "Learn Morgan - HTTP Request logger for NodeJs",
-       body: "Learn Morgan."
-     },
-   ])
- }
-
- // here ! //
-   
+});   
 
 module.exports = router;
